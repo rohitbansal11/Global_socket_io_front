5446bab027ee92ae4ff4e8627261ceba3a92ff21
RohitBansal111 <rohitbansal.eminence@gmail.com>
2022-08-30 12:48:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {socket } from './socket'
+import {useEffect} from 'react'
+import addNotification from 'react-push-notification';
 
 function App() {
+  const getNotificationsEventHandler = (data) => {
+    console.log('jksdfndsfdsnkfnsknfsdfkjsnfkjn')
+    console.log(data)
+    addNotification({
+      title: 'Warning',
+      subtitle: data?.name,
+      message: data?.body,
+      theme: 'darkblue',
+      native: true // when using native, your OS will handle theming.
+  });
+  };
+
+
+  useEffect(() => {
+     socket.on("connection", getNotificationsEventHandler);
+    // socket.on("check", getNotificationsEventHandler);
+   // socket.on("getNotifications", getNotificationsEventHandler);
+    socket.on("getNotifications", getNotificationsEventHandler);
+    // unsubscribe from event for preventing memory leaks
+    return () => {
+      socket.off("getNotifications", getNotificationsEventHandler);
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
